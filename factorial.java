d2db9cf16c3d7317b0bcbc4fbfab84de04e8cf37
Lokesh Kumar <107116502+lokeshkumar111@users.noreply.github.com>
2023-01-24 13:56:16 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,13 @@
+class Main{
+    public static int fact(int n){
+        if(n==1){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+  //driver function
+   public static void main(String[] args){
+      int n = 5;
+      System.out.println(fact(n));
+   }
+}
